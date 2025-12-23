ddfbae6f27fe8a56709dcfafb2b346bc8c36e728
Aaron Tanguma <the_guma@yahoo.com>
2025-12-23 08:09:37 -0700

    created interface, added properties and methods to interface

---
+++ InterfacesDemo/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿namespace InterfacesDemo;
+
+public interface IAnimal
+{
+    // Cannot have fields in an interface
+
+    public int Age { get; set; }
+    public string Diet { get; set; }
+    
+    // interface methods will ALWAYS by stubbed out methods (no scope)
+    // In the background Interfaces are completely abstract (even though we never use the keyword abstract)
+
+    public void Move();
+    public void Breathe();
+}
